6a405ec578b3c895fe524d2de3dbf04342623f57
aleksa-codes <hello@aleksa.codes>
2025-07-16 23:34:59 +0200

    chore: test

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,14 @@
 import { createAuthClient } from 'better-auth/react';
+import { createAuthClient as createVanillaClient } from 'better-auth/client';
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : undefined,
+});
+
+export const { useSession: useVanillaSession } = createVanillaClient({
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : undefined,
+});
+
 export const { signIn, signOut, useSession } = authClient;
 
 export type Session = typeof authClient.$Infer.Session;
--- src/pages/api/auth/[...all].ts
+++ src/pages/api/auth/[...all].ts
@@ -3,6 +3,10 @@ import type { APIRoute } from 'astro';
 
 export const prerender = false; // Not needed in 'server' mode
 
+export const GET: APIRoute = async (ctx) => {
+  return auth.handler(ctx.request);
+};
+
 export const ALL: APIRoute = async (ctx) => {
   return auth.handler(ctx.request);
 };
